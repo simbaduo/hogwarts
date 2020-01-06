f1d3b5a643e635439d740e229c455b937552ee84
Nathanael Swander <swander926@yahoo.com>
2020-01-06 00:04:20 -0500

    fixed landing page bug

--- src/pages/HousePage.jsx
+++ src/pages/HousePage.jsx
@@ -1,33 +1,33 @@
-import React from 'react';
-import axios from 'axios'
-import { Redirect } from 'react-router-dom'
+// import React from 'react';
+// import axios from 'axios'
+// import { Redirect } from 'react-router-dom'
 
 
-const HousePage = () => {
+// const HousePage = () => {
 
-  const [house, setHouse] = useState({ Name: '', Color: ''})
-  const [houseId, setHouseId] = useState(0)
-  const [check, setCheck] = useState(false)
+//   const [house, setHouse] = useState({ Name: '', Color: ''})
+//   const [houseId, setHouseId] = useState(0)
+//   const [check, setCheck] = useState(false)
 
-  const handleChange = x => ({
-    x.persist()
-    setHouse(prevHouse => ({
-    ...prevHouse,
-    []
-    }))
+//   const handleChange = x => ({
+//     x.persist()
+//     setHouse(prevHouse => ({
+//     ...prevHouse,
+//     []
+//     }))
 
-  })
-
-
-
+//   })
 
 
 
-  return (
-    <div>
+
+
+
+//   return (
+//     <div>
       
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 
-export default HousePage;
+// export default HousePage;
